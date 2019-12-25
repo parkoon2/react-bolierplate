16123e94d3ae9d341e1264c194bdd20702995f78
parkoon <devparkoon@gmail.com>
2019-12-25 11:29:19 +0900

    html-loader 와 babel-loader추가 Q. html-loader가 꼭 필요할까? CRA에는 없음. html loader에 minimze 옵션이 있는데, 다른 곳에서도 html을 minimize 할 수 있나?

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,38 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   name: "boilerpate",
-  mode: "development",
+  mode: "production",
   entry: {
     app: path.resolve(__dirname, "src", "index.js")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true }
+          }
+        ]
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: "/node_modules",
+        use: ["babel-loader"]
+      }
+    ]
   },
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist")
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      filename: "index.html"
+    })
+  ]
 };
