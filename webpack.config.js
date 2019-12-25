84f7b2bf575cafc3acae90207c491f13c98b3437
parkoon <devparkoon@gmail.com>
2019-12-25 23:23:47 +0900

    clean webpack plugin
    빌드 경로에 불필요한 파일 제거

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
   name: "boilerpate",
@@ -66,6 +67,7 @@ module.exports = {
     new MiniCssExtractPlugin({
       // output 경로에 생성된다.
       filename: 'style.css'
-    })
+    }),
+    new CleanWebpackPlugin(),
   ]
 };
