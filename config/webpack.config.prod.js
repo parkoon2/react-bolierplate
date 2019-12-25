862bb831620bca84751f880dff3a323b3d7afa6b
parkoon <devparkoon@gmail.com>
2019-12-25 23:31:16 +0900

    webpack을 dev와 prod 나눔
    좀 더 나은 방법을 찾아야할 듯
    (변경이 있으면 두 곳 모두 변경을 해줘야하는 불편...)

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -5,9 +5,9 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
   name: "boilerpate",
-  mode: "development",
+  mode: "production",
   entry: {
-    app: path.resolve(__dirname, "src", "index.js")
+    app: path.join(__dirname, "../src", "index.js")
   },
   module: {
     rules: [
@@ -39,23 +39,7 @@ module.exports = {
   },
   output: {
     filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist")
-  },
-  // development 환경에서만 실행되어야함!
-  // production 에서 실행해도 되지만, 변경 사항이 적용되는데 시간이 오래걸림.
-  devServer: {
-    contentBase: path.resolve(__dirname, 'dist'),
-    // 전체 페이지 리로딩
-    inline: true,
-    // 부분 리로딩
-    hot: true,
-    host: 'localhost',
-    port: 5500,
-    // Enable gzip compression of generated files.
-    compress: true,
-    // It is important to tell WebpackDevServer to use the same "root" path
-    // as we specified in the config. In development, we always serve from /.
-    publicPath: '/',
+    path: path.resolve(__dirname, "../dist")
   },
   plugins: [
     new HtmlWebpackPlugin({
