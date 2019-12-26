f8d66fe040afc7480d11444f0ff72fcb09f1b3bd
parkoon <devparkoon@gmail.com>
2019-12-26 23:46:00 +0900

    webpack - url loader & file-loader 추가

--- frontend/config/webpack.config.base.js
+++ frontend/config/webpack.config.base.js
@@ -54,7 +54,15 @@ module.exports = {
                         loader: 'sass-loader'
                     }
                 ]
-            }
+            },
+            {
+                test: /\.(ico|png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: 'url-loader',
+                options: {
+                    name: '[hash].[ext]',
+                    limit: 10000,
+                },
+            },
         ]
     },
     output: {
